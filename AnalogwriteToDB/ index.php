1f19be907324807d14b9518625c2f683e138cf1b
ihinoken <93440426+ihinoken@users.noreply.github.com>
2022-08-01 18:51:26 +0300

    Add files via upload

---
+++ AnalogwriteToDB/ index.php
@@ -0,0 +1,25 @@
+<?php
+require __DIR__ . "/Inc/bootstrap.php";
+
+if (!file_exists("number.sqlite")) {require_once PROJECT_ROOT_PATH . "/inc/bulidDatabase.php";}
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode("/", $uri);
+
+if ((isset($uri[2]) && $uri[2] != "numbers") || !isset($uri[3]) {
+    header("Location: http://localhost:8080/404.php");
+    exit();
+}
+
+require PROJECT_ROOT_PATH . "/controller/Api/NumberController.php";
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+$objFeedController = new NumberController();
+try {
+    $strMethodName = $uri[3] . 'Action';
+    if ($uri[3] == "post" && $requestMethod == "post") :
+        $objFeedController->post();
+    endif;
+    $objFeedController->{$strMethodName}();
+} catch ( Exception $e ) {
+}
+?>
